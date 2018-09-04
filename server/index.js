3f076c21a5e74109305e540b7c4e5e60cf063c32
Dominic Ginger <domginger@hotmail.co.uk>
2018-09-04 14:01:29 +0100

    Tidy up client side code and simplify codebase

--- server/index.js
+++ server/index.js
@@ -3,6 +3,14 @@ const socket = new WebSocket('ws://localhost:3004')
 socket.onopen = () => console.log('Socket is open')
 socket.onclose = () => console.log('Socket closed')
 
+const sendButton = document.getElementById('sendButton');
+const messageInputBox = document.getElementById('message');
+const receiveBox = document.getElementById('receivebox');
+
+let localConnection, remoteConnection, channel
+
+sendButton.addEventListener('click', sendMessage);
+
 if (window.location.search === '') {
   handleClient1(socket)
 } else {
@@ -10,76 +18,65 @@ if (window.location.search === '') {
   handleClient2(socket)
 }
 
-const disconnectButton = document.getElementById('disconnectButton');
-const sendButton = document.getElementById('sendButton');
-const messageInputBox = document.getElementById('message');
-const receiveBox = document.getElementById('receivebox');
-
-let localConnection, remoteConnection, sendChannel, receiveChannel
-
-disconnectButton.addEventListener('click', disconnectPeers, false);
-sendButton.addEventListener('click', sendMessage, false);
-
-      messageInputBox.disabled = false;
-      sendButton.disabled = false;
-      disconnectButton.disabled = false;
 function handleClient1(socket) {
-  document.querySelector('#connectButton').addEventListener('click', e => {
-    socket.onmessage = ({ data }) => {
-      const msg = JSON.parse(data)
+  socket.onmessage = ({ data }) => {
+    const msg = JSON.parse(data)
 
-      switch(msg.Type) {
-        case 'key':
-          console.log(msg.Key)
-          return
-        case 'answer':
-          localConnection.setRemoteDescription(JSON.parse(msg.Data))
-          return
-        case 'ice':
-          localConnection.addIceCandidate(JSON.parse(msg.Data))
-          return
-      }
+    console.log(msg)
+    switch(msg.Type) {
+      case 'key':
+        return console.log(window.location + "?key=" + msg.Key)
+      case 'answer':
+        return localConnection.setRemoteDescription(JSON.parse(msg.Data))
+      case 'ice':
+        return localConnection.addIceCandidate(JSON.parse(msg.Data))
     }
-    localConnection = new RTCPeerConnection({iceServers: [
-      {urls: ["stun:stun.services.mozilla.com:3478"] },
-    ]});
-    sendChannel = localConnection.createDataChannel("sendChannel");
-    sendChannel.onopen = handleSendChannelStatusChange;
-    sendChannel.onclose = handleSendChannelStatusChange;
-    sendChannel.onmessage = handleReceiveMessage;
+  }
+  localConnection = new RTCPeerConnection({iceServers: [
+    {urls: ['stun:stun.services.mozilla.com:3478'] },
+  ]});
+  channel = localConnection.createDataChannel('channel');
+  channel.onopen = handleChannelStatusChange;
+  channel.onclose = handleChannelStatusChange;
+  channel.onmessage = handleReceiveMessage;
 
-    localConnection.onicecandidate = e => {
-      if (!e.candidate) {
-        return
-      }
-      socket.send(JSON.stringify({
-        Type: 'ice',
-        Data: JSON.stringify(e.candidate),
-        Key: key
-      }))
+  localConnection.onicecandidate = e => {
+    if (!e.candidate) {
+      return
     }
+    socket.send(JSON.stringify({
+      Type: 'ice',
+      Data: JSON.stringify(e.candidate),
+      Key: key
+    }))
+  }
 
-    localConnection.createOffer()
-      .then(offer => localConnection.setLocalDescription(offer))
-      .then(() => socket.send(JSON.stringify({
+  localConnection.createOffer()
+    .then(offer => localConnection.setLocalDescription(offer))
+    .then(() => {
+      const sendOffer = () => socket.send(JSON.stringify({
         Type: 'setOffer',
         Data: JSON.stringify(localConnection.localDescription)
-      })))
+      }))
 
-  })
+      socket.readyState === socket.OPEN ? sendOffer() : socket.onopen = sendOffer
+    })
 }
 
 function handleClient2(socket) {
-  document.querySelector('#connectButton').addEventListener('click', e => {
+  socket.onopen = () => {
     socket.send(JSON.stringify({
       Type: 'getOffer',
       Key: key
     }))
+  }
 
-    socket.onmessage = ({ data }) => {
-      const msg = JSON.parse(data)
+  socket.onmessage = ({ data }) => {
+    const msg = JSON.parse(data)
 
-      if (msg.Type === 'offer') {
+    console.log(msg)
+    switch (msg.Type) {
+      case 'offer':
         remoteConnection.setRemoteDescription(JSON.parse(msg.Data))
         remoteConnection.createAnswer()
           .then(answer => remoteConnection.setLocalDescription(answer))
@@ -90,110 +87,52 @@ function handleClient2(socket) {
               Key: key
             }))
           })
-      } else {
-        remoteConnection.addIceCandidate(JSON.parse(msg.Data))
-      }
-    }
-
-    remoteConnection = new RTCPeerConnection({iceServers: [
-      {url: "stun:stun.services.mozilla.com:3478"},
-    ]});
-    remoteConnection.ondatachannel = receiveChannelCallback;
-
-    remoteConnection.onicecandidate = e => {
-      if (!e.candidate) {
         return
-      }
-      socket.send(JSON.stringify({
-        Type: 'ice',
-        Data: JSON.stringify(e.candidate),
-        Key: key
-      }))
+      case 'ice':
+        return remoteConnection.addIceCandidate(JSON.parse(msg.Data))
     }
-  })
-}
+  }
 
-function handleCreateDescriptionError(error) {
-  console.log("Unable to create an offer: " + error.toString());
-}
+  remoteConnection = new RTCPeerConnection({iceServers: [
+    {url: 'stun:stun.services.mozilla.com:3478'},
+  ]});
+  remoteConnection.ondatachannel = channelCallback;
 
-function handleLocalAddCandidateSuccess() {
-  connectButton.disabled = true;
-}
-
-function handleRemoteAddCandidateSuccess() {
-  disconnectButton.disabled = false;
-}
-
-function handleAddCandidateError() {
-  console.log("Oh noes! addICECandidate failed!");
-}
-
-function sendMessage() {
-  var message = messageInputBox.value;
-  sendChannel && sendChannel.send(message);
-  receiveChannel && receiveChannel.send(message);
-
-  messageInputBox.value = "";
-  messageInputBox.focus();
-}
-
-function handleSendChannelStatusChange(event) {
-  if (sendChannel) {
-    var state = sendChannel.readyState;
-
-    if (state === "open") {
-      messageInputBox.disabled = false;
-      messageInputBox.focus();
-      sendButton.disabled = false;
-      disconnectButton.disabled = false;
-      connectButton.disabled = true;
-    } else {
-      messageInputBox.disabled = true;
-      sendButton.disabled = true;
-      connectButton.disabled = false;
-      disconnectButton.disabled = true;
+  remoteConnection.onicecandidate = e => {
+    if (!e.candidate) {
+      return
     }
+    socket.send(JSON.stringify({
+      Type: 'ice',
+      Data: JSON.stringify(e.candidate),
+      Key: key
+    }))
   }
 }
 
-function receiveChannelCallback(event) {
-  receiveChannel = event.channel;
-  receiveChannel.onmessage = handleReceiveMessage;
-  receiveChannel.onopen = handleReceiveChannelStatusChange;
-  receiveChannel.onclose = handleReceiveChannelStatusChange;
+function sendMessage() {
+  const message = messageInputBox.value;
+  channel.send(message);
+
+  messageInputBox.value = '';
+  messageInputBox.focus();
+}
+
+function channelCallback(event) {
+  channel = event.channel;
+  channel.onmessage = handleReceiveMessage;
+  channel.onopen = handleChannelStatusChange;
+  channel.onclose = handleChannelStatusChange;
 }
 
 function handleReceiveMessage(event) {
-  var el = document.createElement("p");
-  var txtNode = document.createTextNode(event.data);
+  const el = document.createElement('p');
+  const txtNode = document.createTextNode(event.data);
 
   el.appendChild(txtNode);
   receiveBox.appendChild(el);
 }
 
-function handleReceiveChannelStatusChange(event) {
-  if (receiveChannel) {
-    console.log("Receive channel's status has changed to " +
-      receiveChannel.readyState);
-  }
+function handleChannelStatusChange(event) {
+  console.log('WebRTC channel status has changed to ' + channel.readyState);
 }
-
-function disconnectPeers() {
-  sendChannel.close();
-  receiveChannel.close();
-  localConnection.close();
-  remoteConnection.close();
-
-  sendChannel = null;
-  receiveChannel = null;
-  localConnection = null;
-  remoteConnection = null;
-
-  connectButton.disabled = false;
-  disconnectButton.disabled = true;
-  sendButton.disabled = true;
-  messageInputBox.value = "";
-  messageInputBox.disabled = true;
-}
-
